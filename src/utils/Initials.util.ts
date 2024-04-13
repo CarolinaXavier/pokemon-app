783137a394bc77f7b662ebaffc5cfe36ed2d9326
Joel Estumano <carolx54@gmail.com>
2024-04-13 13:07:47 -0300

    Bug Fix: Correção iniciais dos nomes

--- src/utils/Initials.util.ts
+++ src/utils/Initials.util.ts
@@ -2,9 +2,7 @@ export const Initials = (name: string) => {
     const names = name.split("-");
     if (names.length === 1) {
         return names[0].charAt(0).toUpperCase();
-    } else {
-        return (
-            names[0].charAt(0) + names[names.length - 1].charAt(0)
-        ).toUpperCase();
+    } else if (names.length > 1) {
+        return (names[0].charAt(0) + names[1].charAt(0)).toUpperCase();
     }
 };
